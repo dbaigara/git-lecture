ac8ffec70b40b36f0fd232fad15b48529e17fcbf
dbaigara <dbaigara@cern.ch>
2019-09-22 13:10:53 +0300

    Work harded!

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function sayHi() {
     alert("Hello from Git!");
 }
 function work() {
-    alert("Work, work!");
+    alert("Work, work harder!");
     //work in progress...
 }
 function sayBye() {
